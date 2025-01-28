a8638cac23c7ca78ca710d826e6864d4b63392fd
Paritosh <paritosh.kulkarni24@gmail.com>
2025-01-27 19:27:13 -0800

    Refactor

--- Dx12Test/shaders/Common.hlsl
+++ Dx12Test/shaders/Common.hlsl
@@ -48,36 +48,6 @@ ConstantBuffer<CubeConstantBuffer> g_cubeCB : register(b1, space1);
 
 // ---[ Helper Functions ]---
 
-//uint3 Load3x16BitIndices(uint offsetBytes)
-//{
-//    uint3 indices;
-//
-//    // ByteAdressBuffer loads must be aligned at a 4 byte boundary.
-//    // Since we need to read three 16 bit indices: { 0, 1, 2 } 
-//    // aligned at a 4 byte boundary as: { 0 1 } { 2 0 } { 1 2 } { 0 1 } ...
-//    // we will load 8 bytes (~ 4 indices { a b | c d }) to handle two possible index triplet layouts,
-//    // based on first index's offsetBytes being aligned at the 4 byte boundary or not:
-//    //  Aligned:     { 0 1 | 2 - }
-//    //  Not aligned: { - 0 | 1 2 }
-//    const uint dwordAlignedOffset = offsetBytes & ~3;    
-//    const uint2 four16BitIndices = Indices.Load2(dwordAlignedOffset);
-// 
-//    // Aligned: { 0 1 | 2 - } => retrieve first three 16bit indices
-//    if (dwordAlignedOffset == offsetBytes)
-//    {
-//        indices.x = four16BitIndices.x & 0xffff;
-//        indices.y = (four16BitIndices.x >> 16) & 0xffff;
-//        indices.z = four16BitIndices.y & 0xffff;
-//    }
-//    else // Not aligned: { - 0 | 1 2 } => retrieve last three 16bit indices
-//    {
-//        indices.x = (four16BitIndices.x >> 16) & 0xffff;
-//        indices.y = four16BitIndices.y & 0xffff;
-//        indices.z = (four16BitIndices.y >> 16) & 0xffff;
-//    }
-//
-//    return indices;
-//}
 
 inline void GenerateCameraRay(uint2 index, out float3 origin, out float3 direction)
 {
